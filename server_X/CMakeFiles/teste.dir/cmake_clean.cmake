71671cf21406cbc76803f6c3e892a1af79aaa142
tiregram <ruhtra.mar@gmail.com>
2016-11-18 14:00:32 +0100

    xlib

---
+++ server_X/CMakeFiles/teste.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teste.dir/src/X/Xapp.cpp.o"
+  "CMakeFiles/teste.dir/src/test.cpp.o"
+  "bin/teste.pdb"
+  "bin/teste"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teste.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
