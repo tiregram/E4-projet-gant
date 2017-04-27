70e97d0c2476e307dabf5a5418d966ff262e6f18
tiregram <ruhtra.mar@gmail.com>
2017-04-27 15:50:49 +0200

    event

--- server_X/src/Window.hpp
+++ server_X/src/Window.hpp
@@ -6,15 +6,18 @@
 namespace G {
 
   class Geometry;
-
+  class Event;
+  
   class Window
   {
     double xyz[3];
+
   public:
     Window();
     virtual ~Window();
     virtual std::string  getName() const= 0;
-    virtual G::Geometry& get_geo() = 0;
+    virtual G::Geometry& get_geo_manager() = 0;
+    virtual G::Event& get_event_manager() = 0;
     virtual void print(std::ostream&) =0;
   };
 }
--- server_X/src/X/XWindow.cpp
+++ server_X/src/X/XWindow.cpp
@@ -4,6 +4,7 @@
 #include "XWindow.hpp"
 #include "XDisplay.hpp"
 #include "XGeometry.hpp"
+#include "XEvent.hpp"
 
 #include <ostream>
 #include <string>
@@ -18,12 +19,12 @@ namespace Xlib {
   }
 }
 using namespace Xlib;
-G::XWindow::XWindow(XDisplay* display, Xlib::Window win):window_natif(win),display(display),geo(*this)
+G::XWindow::XWindow(XDisplay* display, Xlib::Window win):window_natif(win),display(display),geo(*this),event()
 {
   this->geo.update();
 }
 
-G::Geometry& G::XWindow::get_geo() {
+G::Geometry& G::XWindow::get_geo_manager() {
   return this->geo;
 }
 
@@ -32,9 +33,7 @@ G::XWindow::~XWindow() {
 }
 
 std::string G::XWindow::getName() const {
-  std::string b("TODO");
-
-  return b;
+    return this->getProperties("WM_NAME");
 }
 
 unsigned int G::XWindow::getPID() const{
@@ -66,12 +65,11 @@ unsigned int G::XWindow::getId() const {
 void  G::XWindow::print(std::ostream& os)
 {
   os << "{\"XWindows\":"
-     << "{\"name\":" << this->getName()<<"\""
-     << ",\"geometry\":" << this->get_geo()
+     << "{\"name\":\"" << this->getName()<<"\""
+     << ",\"geometry\":" << this->get_geo_manager()
      << ",\"id:" <<this->getId()
      << " }}";
 }
-
 
 std::ostream& operator<<(std::ostream& os, G::Window* c)
 {
@@ -86,7 +84,8 @@ std::ostream& operator<<(std::ostream& os, G::Window& c)
 }
 
 
-std::shared_ptr<std::string> G::XWindow::getProperties(const std::string& str_NAME)
+
+std::string G::XWindow::getProperties(const std::string& str_NAME) const 
 {
   Atom  prop_type, prop, da;
   char *an;
@@ -98,18 +97,26 @@ std::shared_ptr<std::string> G::XWindow::getProperties(const std::string& str_NA
   prop_type = Xlib::XInternAtom(this->display->xdisplay_natif, str_NAME.c_str(), True);
 
   status = Xlib::XGetWindowProperty(this->display->xdisplay_natif, this->window_natif, prop_type, 0L, sizeof (Xlib::Atom), False,
-                                    XA_CARDINAL, &da, &di, &dl, &dl2, &prop_ret);
+                                    XA_STRING, &da, &di, &dl, &dl2, &prop_ret);
 
   if (status == Success && prop_ret )
     {
-      std::cout << "status:" << status
-                << ", " << da
-                << ", " << di
-                << ", " << dl
-                << ", " << dl2
-                << ", " << *((long*)prop_ret)
-                << "\n";
+      // std::cout << "status:" << status
+      //           << ", " << da
+      //           << ", " << di
+      //           << ", " << dl
+      //           << ", " << dl2
+      //           << ", " << *((long*)prop_ret)
+      //           << "\n";
+      return std::string((char*)prop_ret);
     }
+
+  return "undef";
 
 }
 
+G::Event& G::XWindow::get_event_manager()
+{
+
+  return this->event;;
+}
