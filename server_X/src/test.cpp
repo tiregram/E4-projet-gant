85e99b772ccbd45de2a39c479080687165d272a8
ThibaultVigier <thibault.vigier.45@gmail.com>
2017-04-28 17:08:18 +0200

    Connection SSH / Exec OK

--- server_X/src/test.cpp
+++ server_X/src/test.cpp
@@ -38,7 +38,10 @@ int main(int argc, char* argv[]) {
 	std::shared_ptr<G::Launcher> launcher;
 	try {
 		launcher = std::make_shared<G::Launcher>();
+
 		int rc = launcher->show_remote_files();
+		// int rc = launcher->shell_session();
+
 		std::cout << "Return Code From SSH Connection" << rc << '\n';
 	} catch(std::exception err) {
 		std::cerr << err.what() << "\n";
