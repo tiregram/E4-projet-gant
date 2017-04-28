8843b136e676b3c76d5606e8943b4050c2a7e331
tiregram <ruhtra.mar@gmail.com>
2017-04-28 15:30:41 +0200

    mise en place event secu

--- server_X/src/X/XEvent.cpp
+++ server_X/src/X/XEvent.cpp
@@ -1,7 +1,16 @@
 #include "XEvent.hpp"
+#include "XWindow.hpp"
+#include "XDisplay.hpp"
 #include <iostream>
 
-G::XEvent::XEvent():Event() {
+namespace Xlib {
+  extern "C" {
+#include <X11/X.h>
+#include <X11/Xutil.h>
+  }
+}
+
+G::XEvent::XEvent(XWindow& w):Event(),window(w) {
 
 }
 
@@ -11,4 +20,21 @@ G::XEvent::~XEvent() {
 
 void  G::XEvent::key_press(char C) const {
   std::cout << "key event send"  << "\n";
+
+  int propagate =  true;
+  long event_mask = KeyPressMask;
+
+  Xlib::XEvent event_send;
+  Xlib::XKeyPressedEvent *a;
+  a = &event_send.xkey;
+
+
+  Status result =  Xlib::XSendEvent(
+                                    this->window.get_display()->xdisplay_natif,
+                                    this->window.get_window_natif(),
+                                    propagate,
+                                    event_mask,
+                                    &event_send);
+
+  
 }
