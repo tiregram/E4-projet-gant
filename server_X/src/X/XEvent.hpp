2a3fa55aaae95de015cff8ad25aa37f1843bacd9
tiregram <ruhtra.mar@gmail.com>
2017-04-28 15:30:41 +0200

    mise en place event secu

--- server_X/src/X/XEvent.hpp
+++ server_X/src/X/XEvent.hpp
@@ -3,11 +3,16 @@
 #include "../Event.hpp"
 
 namespace G{
+  class XWindow;
+
   class XEvent : public G::Event
   {
+  private:
+
+    XWindow& window;
 
   public:
-    XEvent();
+    XEvent(XWindow&);
     virtual ~XEvent();
 
     void key_press(char C) const override;
--- server_X/src/test.cpp
+++ server_X/src/test.cpp
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 
     for (auto a : di->get_windows_list()) {
       std::cout << *a << "\n"
-                <<"try to change size... \n";
-
+                <<"event... \n";
+      a->get_event_manager().key_press('a');
 
       // G::Geometry& g = a->get_geo_manager();
       // g.set_width(100);
