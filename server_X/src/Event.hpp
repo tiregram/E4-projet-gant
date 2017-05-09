c8570f8c5822b323a5cdc2ec7f1d5f87fcd44ab8
tiregram <ruhtra.mar@gmail.com>
2017-05-09 17:53:44 +0200

    mise en place mouse et key

--- server_X/src/Event.hpp
+++ server_X/src/Event.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 namespace G {
+
   class Event
   {
   public:
@@ -8,5 +9,9 @@ namespace G {
     virtual ~Event();
 
     virtual void key_press(char C) const=0;
+    virtual void key_release(char C) const=0;
+
+    virtual void mouse_press() const=0;
+    virtual void mouse_release() const=0;
   };
 }
---
+++ troisdhetWMUnreal 4.15/Source/E4FI/E4FI.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "E4FI.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, E4FI, "E4FI" );
